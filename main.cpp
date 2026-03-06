2bd012f5e8972f5a1ac3df36a83cd66040e61791
joelle0409 <rinzu11mj@gmail.com>
2026-03-06 11:07:19 +0100

    Added lines

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-
+// bool: true or false. Currently false until the keys are pressed, then it will be true.
 struct TickInput
 {
 	bool up = false;
@@ -28,6 +28,10 @@ enum class LevelId { L1, L2, L3};
 
 struct GameState
 {
+	// vector as a dynamic list, a list of Pos values for both Snake and walls, store many positions.
+	// the snake is a multiple body parts, with positions.
+	// same goes for the walls.
+
 	std::vector<Pos> snake;
 	std::vector<Pos> walls;
 
@@ -45,6 +49,10 @@ struct GameState
 
 static LevelId ParseLevel(int argc, char** argv)
 {
+	// Parse means to read some text and interpret what it means.
+	// argc is how many arguments there are, argv is the actual argument text.
+	// (char& c : arg) loops through every character string of "LEVEL2", turning it into "level2". tolower.
+
 	if (argc < 2) return LevelId::L1;
 
 	std::string arg = argv[1];
@@ -59,34 +67,43 @@ static LevelId ParseLevel(int argc, char** argv)
 
 static bool IsWall(const GameState& game, int x, int y)
 {
+	// check if there is a wall on x, y
+	// for every wall position inside game.walls, call it w.
+	
 	for (const Pos& w : game.walls)
-		if (w.x == x && w.y == y) return true;
+		if (w.x == x && w.y == y) return true; // if all are equal to one another then they are the same position.
 	return false;
 }
 
 static void BuildLevel(GameState& game, LevelId level)
 {
+	// remove any old walls first when restarting or changing level.
+	// cx, cy means the middle of the map. 
 	game.walls.clear();
 
 	int cx = game.width / 2;
 	int cy = game.height / 2;
 
+	// if the chosen level is level2, the build level2 otherwise, level3.
 	if (level == LevelId::L2)
 	{
 		for (int y = cy - 4; y <= cy + 4; y++)
-			game.walls.push_back({ cx, y });
+			game.walls.push_back({ cx, y }); // for each y values, add a wall at x = center.
 	}
 	else if (level == LevelId::L3)
 	{
-		//forming the T
+		// forming the T
+		// halfBar = 6, controls half the width of the top T
+		// topY = cy - 4, moves top T a bit above the middle.
+
 		int halfBar = 6;
 		int topY = cy - 4;
 
-		for (int x = cx - halfBar; x <= cx + halfBar; x++)
+		for (int x = cx - halfBar; x <= cx + halfBar; x++) // draws the top horizontal of T
 		{
 			game.walls.push_back({ x, topY });
 		}
-		for (int y = topY; y <= cy + 6; y++)
+		for (int y = topY; y <= cy + 6; y++) // draws the vertical of T
 		{
 			game.walls.push_back({ cx, y });
 		}
@@ -116,13 +133,17 @@ static void RespawnFood(GameState& game)
 
 static Pos GetNextHead(const GameState& game)
 {
+	// tAKE THe current head position. the head is stored at index 0
 	Pos head = game.snake[0];
 
+	// if the snake is moving up, subtract 1 from y
+	// same goes for other directions with -- or ++
 	if (game.dir == Dir::Up) head.y--;
 	else if (game.dir == Dir::Down) head.y++;
 	else if (game.dir == Dir::Left) head.x--;
 	else if (game.dir == Dir::Right) head.x++;
 
+	// return the new calculated pos
 	return head;
 }
 
@@ -133,12 +154,7 @@ int main(int argc, char** argv)
 	srand((unsigned int)time(NULL));
 	LevelId chosenLevel = ParseLevel(argc, argv);
 
-	if (chosenLevel == LevelId::L1)
-		std::cout << "Level 1 loaded" << std::endl;
-	else if (chosenLevel == LevelId::L2)
-		std::cout << "Level 2 loaded" << std::endl;
-	else if (chosenLevel == LevelId::L3)
-		std::cout << "Level 3 loaded" << std::endl;
+	
 
 	while (true)
 	{
@@ -158,8 +174,15 @@ int main(int argc, char** argv)
 		BuildLevel(game, chosenLevel);
 		RespawnFood(game);
 
+		// Tick: one cycle of the game loop, one game step.
+		// The snake will move once every loop, then moves one tile per tick
 		while (!game.gameOver)
 		{
+			//INPUT: 
+			
+			// WASD buttons
+			// Information gets stored in TickInput input; allowing the game to know what the player wants to do.
+
 			input.up = getIfBasicKeyIsCurrentlyDown('W') || getIfUpKeyIsCurrentlyDown();
 			input.down = getIfBasicKeyIsCurrentlyDown('S') || getIfDownKeyIsCurrentlyDown();
 			input.left = getIfBasicKeyIsCurrentlyDown('A') || getIfLeftKeyIsCurrentlyDown();
@@ -170,24 +193,31 @@ int main(int argc, char** argv)
 			if (input.quit)
 				break;
 
-			//DIRECTION:
+			// LOGIC: 
+
+			// changes the snake direction, but preventing turining backwards.
+
 			if (input.up && game.dir != Dir::Down) game.dir = Dir::Up;
 			else if (input.down && game.dir != Dir::Up) game.dir = Dir::Down;
 			else if (input.left && game.dir != Dir::Right) game.dir = Dir::Left;
 			else if (input.right && game.dir != Dir::Left) game.dir = Dir::Right;
 
+			// determines where the snake will move next. 
 			Pos newHead = GetNextHead(game);
 
+			// snake hits edge, game over. 
 			if (newHead.x <= 0 || newHead.x >= game.width - 1 || newHead.y <= 0 || newHead.y >= game.height - 1)
 			{
 				game.gameOver = true;
 			}
 
+			// same here, game over. 
 			if (IsWall(game, newHead.x, newHead.y))
 			{
 				game.gameOver = true;
 			}
 
+			// check if the snake had taken the food 
 			bool ateFood = (newHead.x == game.food.x && newHead.y == game.food.y);
 
 			if (!game.gameOver)
@@ -204,6 +234,7 @@ int main(int argc, char** argv)
 
 			if (!game.gameOver)
 			{
+				// GROW! and gain score.
 				if (ateFood)
 				{
 					game.score++;
@@ -213,6 +244,7 @@ int main(int argc, char** argv)
 				}
 				else
 				{
+				// add new head, remove tail, snake moves forward.
 					game.snake.insert(game.snake.begin(), newHead);
 					game.snake.pop_back();
 				}
@@ -220,9 +252,11 @@ int main(int argc, char** argv)
 
 
 			//RENDER AREA:
+
+			// erase previous frame 
 			clearBuffer();
 
-			//walls
+			// walls
 			for (int x = 0; x < game.width; x++)
 			{
 				drawTile(x, 0, '#');
@@ -235,25 +269,31 @@ int main(int argc, char** argv)
 				drawTile(game.width - 1, y, '#');
 			}
 
+			// snake, @ as the head, o as the body
 			for (int i = 0; i < (int)game.snake.size(); i++)
 			{
 				char c = (i == 0) ? '@' : 'o';
 				drawTile(game.snake[i].x, game.snake[i].y, c);
 			}
 
+			// draw level walls with BuildLevel
 			for (const Pos& w : game.walls)
 			{
 				drawTile(w.x, w.y, '#');
 			}
 
+			// Food is going to be * in the game.
+			// get food and score is displayed. 
 			drawTile(game.food.x, game.food.y, '*');
 			drawString(1, 1, (std::string("Snake - Score: ") + std::to_string(game.score)).c_str());
 
 			if (game.gameOver)
 				drawString(10, 10, "GAME OVER");
 
+			// send everything drawn to the screen.
 			renderBuffer();
 
+			// controls how fast each tick runs
 			int speed = 150 - game.score * 5;
 			if (speed < 60) speed = 60;
 			Sleep(speed);
@@ -261,6 +301,8 @@ int main(int argc, char** argv)
 
 		clearBuffer();
 		drawString(1, 1, (std::string("Snake - Score: ") + std::to_string(game.score)).c_str());
+
+		// line pos
 		drawString(10, 10, "GAME OVER");
 		drawString(10, 12, "PRESS ESC to exit");
 		drawString(10, 13, "PRESS R to restart");
